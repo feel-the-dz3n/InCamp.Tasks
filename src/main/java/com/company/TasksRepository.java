cf6bd39b224de23fb2b072101be22263648bcb7a
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-24 12:13:18 +0200

    Implement removeTask, updateTask, addTask

--- src/main/java/com/company/TasksRepository.java
+++ src/main/java/com/company/TasksRepository.java
@@ -13,15 +13,51 @@ public class TasksRepository {
     }
 
     public Task addTask(Task task) throws Exception {
-        throw new Exception("Not implemented");
+        var conn = database.connect();
+
+        var stmt = conn.prepareStatement("INSERT INTO public.tasks(\"Title\", \"Description\", \"IsDone\", \"DueDate\") VALUES (?, ?, ?, ?) RETURNING \"Id\";");
+        stmt.setString(1, task.title);
+        stmt.setString(2, task.description);
+        stmt.setBoolean(3, task.done);
+        stmt.setDate(4, Date.valueOf(task.dueDate));
+
+        int id = stmt.executeUpdate();
+        task.id = id;
+
+        stmt.close();
+        conn.close();
+
+        return task;
     }
 
     public Task updateTask(Task task) throws Exception {
-        throw new Exception("Not implemented");
+        var conn = database.connect();
+
+        var stmt = conn.prepareStatement(String.format(
+                "UPDATE public.tasks SET \"Title\"=?, \"Description\"=?, \"IsDone\"=?, \"DueDate\"=? WHERE \"Id\"=%d;",
+                task.id));
+
+        stmt.setString(1, task.title);
+        stmt.setString(2, task.description);
+        stmt.setBoolean(3, task.done);
+        stmt.setDate(4, Date.valueOf(task.dueDate));
+
+        stmt.executeUpdate();
+
+        stmt.close();
+        conn.close();
+
+        return task;
     }
 
     public void removeTask(Integer id) throws Exception {
-        throw new Exception("Not implemented");
+        var conn = database.connect();
+        var stmt = conn.createStatement();
+
+        stmt.execute(String.format("DELETE FROM public.tasks WHERE \"Id\" = %d;", id));
+
+        stmt.close();
+        conn.close();
     }
 
     public void removeTask(Task task) throws Exception {
